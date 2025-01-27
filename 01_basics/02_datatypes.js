2c67e81ca823e10172d4ce01e43a2e528d3163c3
Satyaprakash-136 <151207848+Satyaprakash-136@users.noreply.github.com>
2025-01-27 19:02:03 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+//alert("Hello") // we are using node JS not browser
+
+console.log(3+3)  // code readability should be high
+console.log("HEllo")
+
+let name = "age"
+let age = 18
+let isLoggedIn = false
+let state;  //datatype => undefined
+
+
+// number range => 2 to power 53
+// biging
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not asigned
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
