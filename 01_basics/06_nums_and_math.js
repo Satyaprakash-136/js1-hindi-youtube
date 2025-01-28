9255e1f6310fe8b0a458681f8d6917ecb69f60a6
Satyaprakash-136 <151207848+Satyaprakash-136@users.noreply.github.com>
2025-01-28 16:39:00 +0000

    Math and numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+// const score = 400
+// console.log(score) //400 
+
+/*const balance = new Number(100)
+console.log(balance) // [Number: 100]
+console.log(balance.toString().length) //3
+console.log(balance.toFixed(1)) //100.0*/
+
+/*
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));//123.9
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));//10,00,000 */
+
+
+// console.log(Math); //Object [Math] {}
+// console.log(Math.abs(-4)); //4
+// console.log(Math.round(4.6)); //5
+// console.log(Math.ceil(4.2)); //5
+// console.log(Math.floor(4.9));  //4
+// console.log(Math.min(4, 3, 6, 8));  //3
+// console.log(Math.max(4, 3, 6, 8));  //8
+
+// console.log(Math.random()); //The Math.random() static method returns a floating-point,
+                            //  pseudo-random number that's greater than or equal to 0 and less than 1,
+                            // with approximately uniform distribution over that range — which you can 
+                            // then scale to your desired range. The implementation selects the initial 
+                            // seed to the random number generation algorithm; it cannot be chosen or reset by the user.
+
+
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
